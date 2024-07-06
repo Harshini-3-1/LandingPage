57e20bd7a750cd8fba96174d020f84fa73e13c18
Harshini-3-1 <harshinichowdary31@gmail.com>
2024-07-06 11:21:14 +0530

    LandingPage

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('lawsuit-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your submission. We will contact you soon.');
+});
